ccac43bc15dac3f264fbd679d406ebf70bf37770
joker45op <85115298+joker45op@users.noreply.github.com>
2023-05-04 16:32:07 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# test
-Created with CodeSandbox
+
+# Instructions:
+View the various feature branches to see source code of the various examples, linked below:
+https://github.com/LiamOsler/three-react-cannon-examples/branches
+
+## Demonstrations completed to date:
+* Setup:
+  - Loading models using .gtlf files
+  - Instancing objects in a scene
+  - Lighting effects and fogs
+* Physics:
+  - Cubes
+  - Other primitives
+  - Convex Hull Shapes
+  - Trimesh
+
+## Loading Models:
+### GLTF Loader:
+Git Repo: https://github.com/LiamOsler/three-react-cannon-examples/tree/GLTFLoader
+
+Live Demo: https://liamosler.ca/demos/threejs/GLTFLoader/
+
+
+### Instancing objects in a GLTF file:
+Git Repo: https://github.com/LiamOsler/three-react-cannon-examples/tree/GLTFInstancing
+
+## Physics:
+### Three-Cannon:
+#### Cubes:
+Git Repo: https://github.com/LiamOsler/three-react-cannon-examples/tree/cubes
+
+Live Demo: https://liamosler.ca/demos/threejs/boxes-cannon/
+
+#### Trimesh collisions:
+Git Repo: https://github.com/LiamOsler/three-react-cannon-examples/tree/trimesh
+
+Live Demo: https://liamosler.ca/demos/threejs/trimesh-cannon/
+
+# React App installation instructions:
+## Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.
---
+++ src/App.js
@@ -0,0 +1,95 @@
+import './App.css';
+
+
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState, Suspense } from 'react'
+import { extend, Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Physics, usePlane, useTrimesh } from '@react-three/cannon'
+import { threeToCannon, ShapeType } from 'three-to-cannon';
+
+import suzanne from './suzanne.gltf'
+
+
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, domElement]} />;
+};
+
+
+function Suzanne(props) {
+  const result = threeToCannon(props.model, {type: ShapeType.MESH});
+  const [ref] = useTrimesh(() => ({ args: [result.shape.vertices, result.shape.vertices], mass: 10, position: props.position, rotation: props.rotation}))
+  return( 
+    <mesh >
+      <primitive ref={ref} object={props.model}  />
+      <meshNormalMaterial />
+    </mesh>
+  )
+}
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
+  return (
+    <mesh ref={ref}>
+      <planeGeometry args={[100, 100]} />
+    </mesh>
+  )
+}
+
+
+function App() {
+  const gltf = useLoader(GLTFLoader, suzanne)
+  const suzanneMesh = gltf.scene.children[2]
+  const suzanneObjects = []
+  for(let i = -2; i < 2; i ++){
+    for(let j = -2; j < 2; j ++){
+      const suzanneClone = suzanneMesh.clone()
+      suzanneObjects.push(
+        <Suzanne position = {[i*10, 3, j*10]} rotation = {[i/4, j/4, i*j/4]} model = {suzanneClone}/>
+      )
+    }
+  } 
+  return (
+    
+    <div className="App">
+
+      <header className="App-header">
+
+      </header>
+
+      <div className="App-background">
+        <div className = "App-body">
+          
+          <Canvas >
+            <ambientLight intensity={0.5} />
+            <pointLight color="white" intensity={1} position={[10, 10, 10]} />
+            <CameraControls />
+            <Physics>
+              <Plane />
+              {suzanneObjects}
+            </Physics>
+          </Canvas>
+        
+        </div>
+
+        <div className= "App-overlay">
+        </div>
+      </div>
+      
+
+    </div>
+  );
+}
+
+export default App;
+useGLTF.preload("/suzanne.gltf");
